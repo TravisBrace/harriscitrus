f4e643469035096e77643447a970dd9108773862
Travis <TravisBrace@gmail.com>
2014-08-23 21:08:19 -0400

    removed turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require jquery.turbolinks
+//= require bootstrap
+
 //= require jquery.backstretch.min
 //= require simpleCart.min
 //= require_tree .
