788b9981ce50c5d349ee5773036645d88e3c707e
Travis <TravisBrace@gmail.com>
2014-09-08 23:37:25 -0400

    updated product views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,9 @@ class CategoriesController < ApplicationController
   def show
     @category=Category.find(params[:id])
   end
+  def index
+    @category = Category.all
+  end
   private
   def category_params
     params.require(:category).permit(:name)
