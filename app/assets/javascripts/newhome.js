e25a355d0b4e0322c15eab04075845c10f85d5f7
bractr <TravisBrace@gmail.com>
2016-05-18 19:48:25 -0400

    updated jawns

--- app/assets/javascripts/newhome.js
+++ app/assets/javascripts/newhome.js
@@ -3,7 +3,14 @@ $(document).ready(function(){
     $('.background-slideshow div:gt(0)').hide();
     setInterval(function(){
       $('.background-slideshow div:first').fadeOut(1000)
-        .next('div').fadeIn(700)
+        .next('div').fadeIn(3000)
         .end().appendTo('.background-slideshow');}, 7000);
   });
 });
+
+  $(window).on("scroll", function(){
+    if($("body").scrollTop() >= $(".slide1").height() - 212){
+      $(".container").removeClass("hide");
+      $(".tree_trio").removeClass("hide");
+    };
+  });
