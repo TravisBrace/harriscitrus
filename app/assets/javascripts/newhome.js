ac285e19b7b39e3f6dd1b7979991c8a394e1d028
bractr <TravisBrace@gmail.com>
2016-06-06 09:03:56 -0400

    updated slide 2 parallax scrolling

--- app/assets/javascripts/newhome.js
+++ app/assets/javascripts/newhome.js
@@ -1,16 +1,24 @@
 $(document).ready(function(){
   $(function(){
-    $('.background-slideshow div:gt(0)').hide();
+    $('.background-slideshow div:gt(0)').addClass('fadeout');
     setInterval(function(){
-      $('.background-slideshow div:first').fadeOut()
-        .next('div').fadeIn(2000)
+      $('.background-slideshow div:first').addClass('fadeout')
+        .next('div').removeClass('fadeout', 2000)
         .end().appendTo('.background-slideshow');}, 8000);
   });
 });
 
-  $(window).on("scroll", function(){
+  $(window).on("scroll", function (){
     if($(document).scrollTop() >= $(".slide1").height() - 212){
       $(".container2").removeClass("containertransformX")
       $(".tree_trio").animate({left: '0'}, 1000);
     };
+  });
+
+
+  $(window).on("scroll", function(){
+    var ypos, slide2, parallax;
+    ypos = window.pageYOffset;
+    slide2 = document.getElementById('slide2');
+    slide2.style.top = ypos * .4 + 'px';
   });
